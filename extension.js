0bf54f1d6b9a548319a47e5b3240c8c589f3e395
Gavin Henry <ghenry@suretecsystems.com>
2013-03-28 20:10:14 +0000

    Add GPLv3

--- extension.js
+++ extension.js
@@ -1,3 +1,22 @@
+//
+// Copyright (c) 2013, Suretec Systems Ltd. L<http://www.suretecsystems.com/>
+//
+// This file is part of the SureVoIP Browser Plugin
+//
+// The SureVoIP Browser Plugin is free software: you can redistribute it 
+// and/or modify it under the terms of the GNU General Public License as 
+// published by the Free Software Foundation, either version 3 of the 
+// License, or (at your option) any later version.
+//
+// The SureVoIP Browser Plugin is distributed in the hope that it will be 
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the SureVoIP Browser Plugin.  If not, see 
+// <http://www.gnu.org/licenses/>.
+//
 // Used to avail $ outside of appAPI.ready
 var $ = undefined;
 
